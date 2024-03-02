6771f348a4f839bd150c61af5597b6a3af45c9d7
chun1 <arthur@chunai.dev>
2024-03-02 17:39:21 +1100

    refactor layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,48 +2,30 @@ import { Post } from "@prisma/client";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
-import {
-  IoIosNotificationsOutline as BellClose,
-  IoMdNotifications as BellOpen,
-} from "react-icons/io";
-import { PiArticleThin as StoryIcon } from "react-icons/pi";
-
-import {
-  CiSearch as SearchIcon,
-  CiBookmark as LibraryIcon,
-} from "react-icons/ci";
-import { PiNotePencilThin as WriteIcon } from "react-icons/pi";
-import { RxPerson as ProfileIcon } from "react-icons/rx";
-
-import { IoStatsChartOutline as StatsIcon } from "react-icons/io5";
+import { IoMdNotifications as BellOpen } from "react-icons/io";
 
 import { useState } from "react";
 
 import { api } from "~/utils/api";
 import { Bell } from "lucide-react";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuPortal,
-  DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuSub,
   DropdownMenuSubContent,
   DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
 import { SplashBackground } from "../components/svgs/SplashBackground";
-import { Logo } from "../components/svgs/Logo";
+import { Logo } from "../components/svgs/logo";
+import { Navbar } from "~/components/layout";
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+  const hello = api.post.check.useQuery({ text: "from tRPC" });
   const [blogPost, setBlogPost] = useState<Post[]>([]);
 
   const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+  const { data: secretMessage } = api.post.checkSecret.useQuery(
     undefined, // no input
     { enabled: sessionData?.user !== undefined },
   );
@@ -57,91 +39,16 @@ export default function Home() {
 
       {sessionData ? (
         <>
-          <nav
-            className="flex w-full items-baseline justify-between p-4   text-slate-500
-        "
-          >
-            <div className="flex  items-center gap-3">
-              <Link href="" className="h-7  w-11 overflow-hidden rounded-full">
-                <Logo />
-                {/* search input */}
-              </Link>
-              <form className="flex gap-2 rounded-full bg-gray-100 p-3">
-                <SearchIcon className="h-6 w-6" />
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="border-none bg-transparent placeholder-slate-500 outline-none"
-                />
-              </form>
-            </div>
-
-            <div className="flex items-center gap-5 pt-1 text-lg">
-              <Link
-                href="write"
-                className="flex items-center justify-center gap-1 "
-              >
-                <WriteIcon className="h-7 w-7" /> Write
-              </Link>
-              <Link href="me/notifications">
-                <BellClose className="h-7 w-7" />
-              </Link>
-              {/* profile dropdown */}
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <div className=" h-8 w-8  gap-2 overflow-hidden rounded-full">
-                    {sessionData?.user?.image ? (
-                      <img src={sessionData.user?.image} className="w-full" />
-                    ) : (
-                      <div
-                        className="text-xl font-bold text-white"
-                        // random hue background
-                        style={{
-                          background: `hsl(${Math.floor(
-                            Math.random() * 360,
-                          )}, 100%, 50%)`,
-                        }}
-                      >
-                        {/*Placeholder same as youtube  */}
-                        {sessionData?.user?.name?.[0] ?? ""}
-                      </div>
-                    )}
-                  </div>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56  p-4 text-lg font-thin  text-gray-600 hover:text-gray-700">
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                      <ProfileIcon className="mr-3 h-5 w-5" />
-                      Profile
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <LibraryIcon className="mr-3 h-5 w-5" />
-                      <span>Library</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <StoryIcon className="mr-3 h-5 w-5" />
-                      <span>Story</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <StatsIcon className="mr-3 h-5 w-5" />
-                      <span>Stats</span>
-                    </DropdownMenuItem>
-                  </DropdownMenuGroup>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem>Settings</DropdownMenuItem>
-                  </DropdownMenuGroup>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          </nav>
+          <Navbar />
+          <main className="container p-3">
           <h1>Secret Message</h1>
           <p>{secretMessage}</p>
+          </main>
         </>
       ) : (
         <>
           <header
-            className="bg-orange flex w-full items-baseline justify-between   p-4
+            className="flex w-full items-baseline justify-between bg-orange   p-4
         "
           >
             <Link href="">
@@ -159,7 +66,7 @@ export default function Home() {
               </button>
             </div>
           </header>
-          <main className="  bg-orange flex justify-between  border-y-[1px] border-black px-3">
+          <main className="  flex justify-between border-y-[1px]  border-black bg-orange px-3">
             <div className="container right-0 flex max-w-md flex-col items-center justify-center gap-12 px-4 py-24">
               <h1 className="text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
                 Stay curious
@@ -186,7 +93,7 @@ export default function Home() {
 function AuthShowcase() {
   const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+  const { data: secretMessage } = api.post.checkSecret.useQuery(
     undefined, // no input
     { enabled: sessionData?.user !== undefined },
   );
