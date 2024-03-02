d4ab9f76ba81f58a8652407f7a20fbd7872da53d
chun1 <arthur@chunai.dev>
2024-03-02 15:26:08 +1100

    add mdx editor to transpile

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  transpilePackages: ["@mdxeditor/editor"],
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
