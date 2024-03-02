79575c6392b715d843835b1be453edda8e092b57
chun1 <arthur@chunai.dev>
2024-03-03 09:06:37 +1100

    post page initial end point and debug head

---
+++ src/pages/[user]/[post].tsx
@@ -0,0 +1,30 @@
+import { useRouter } from "next/router";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { api } from "~/utils/api";
+import { Navbar } from "~/components/layout";
+
+export default function Page() {
+  // TODO layouts
+  const router = useRouter();
+  // check if user is the one signed in
+  const { data: sessionData } = useSession();
+  // if (!sessionData) {
+  //   signIn();
+  // }
+  // const { user, error } = router.query;
+  console.log(sessionData);
+  console.log(router.query);
+  const { data } = api.post.getPost.useQuery({
+    id: parseInt(router.query.post as string),
+  });
+
+  return (
+    <>
+      <p>Post: {router.query.user}</p>
+      <p>Post: {router.query.post}</p>
+      <pre>{JSON.stringify(data, null, 4)}</pre>
+      <Navbar />
+      <h1>{data?.title}</h1>
+    </>
+  );
+}
