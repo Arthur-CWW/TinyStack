155d9f9b77774e49b62268dde3953103f93d9262
chun1 <arthur@chunai.dev>
2024-03-02 17:36:40 +1100

    added blog upload backend

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,3 +1,5 @@
+import { Category } from "@prisma/client";
+import { title } from "process";
 import { z } from "zod";
 
 import {
@@ -5,9 +7,67 @@ import {
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
+import { fakePost } from "~/utils/data";
+
+const CategorySchema = z.nativeEnum(Category);
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
+  createPost: protectedProcedure
+    .input(
+      z.object({
+        // name: z.string().min(6),
+        // category: z.string().min(1),
+        title: z.string().min(6),
+        category: CategorySchema,
+        content: z.string().min(6),
+        tags: z.array(z.string()),
+        published: z.boolean(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      // simulate a slow db call
+      // await new Promise((resolve) => setTimeout(resolve, 100));
+      console.log("input", input);
+
+      return ctx.db.post.create({
+        data: {
+          title: input.title,
+          category: input.category,
+          content: input.content,
+          tags: {
+            create: input.tags.map((tag) => ({ name: tag })),
+          },
+          published: input.published,
+          authorId: ctx.session.user.id,
+          // comments: { create: [] },
+          // createdBy: { connect: { id: ctx.session.user.id } },
+        },
+      });
+    }),
+
+  getUserPosts: publicProcedure
+    .input(z.object({ author: z.string() }))
+    .query(({ input, ctx }) => {
+      return ctx.db.post.findMany({
+        where: { author: { name: input.author } },
+      });
+    }),
+
+  // top blog posts
+  getLatest: publicProcedure.query(({ ctx }) => {
+    return ctx.db.post.findFirst({
+      orderBy: { createdAt: "desc" },
+      // where: { createdBy: { id: ctx.session.user.id } },
+    });
+  }),
+
+  getblogPost: publicProcedure
+    .input(z.object({ nPosts: z.number() }))
+    .query(({ input }) => {
+      return Array.from({ length: input.nPosts }, () => fakePost());
+    }),
+
+  check: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
       return {
@@ -15,28 +75,7 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-          createdBy: { connect: { id: ctx.session.user.id } },
-        },
-      });
-    }),
-
-  getLatest: protectedProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-      where: { createdBy: { id: ctx.session.user.id } },
-    });
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
+  checkSecret: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
 });
