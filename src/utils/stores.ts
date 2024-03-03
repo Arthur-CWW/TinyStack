7a6aa70faaf0dcb23b450d30851bdd65f19a84d9
chun1 <arthur@chunai.dev>
2024-03-04 09:52:36 +1100

    fix zustand usage

--- src/utils/stores.ts
+++ src/utils/stores.ts
@@ -1,44 +1,11 @@
 import { useState, useEffect } from "react";
 
-export const useStore = <T, F>(
-  store: (callback: (state: T) => unknown) => unknown,
-  callback: (state: T) => F,
-) => {
-  const result = store(callback) as F;
-  const [data, setData] = useState<F>();
-
-  useEffect(() => {
-    setData(result);
-  }, [result]);
-
-  return data;
-};
-// useBearStore.ts
-
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 interface BearState {
   bears: number;
   increase: (by: number) => void;
 }
-
-// the store itself does not need any change
-// export const useBearStore = create<BearState>(
-//   persist(
-//     (set, get) => ({
-//       bears: 0,
-//       addABear: () => set({ bears: get().bears + 1 }),
-//     }),
-//     {
-//       name: "food-storage",
-//     },
-//   ),
-// );
-
-// const useBearStore = create<BearState>()((set) => ({
-//   bears: 0,
-//   increase: (by) => set((state) => ({ bears: state.bears + by })),
-// }));
 
 export const useBearStore = create<BearState>()(
   devtools(
@@ -51,8 +18,3 @@ export const useBearStore = create<BearState>()(
     ),
   ),
 );
-
-// export const useBearStore = create<BearState>()((set) => ({
-//   bears: 0,
-//   increase: (by) => set((state) => ({ bears: state.bears + by })),
-// }));
