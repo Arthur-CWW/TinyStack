31d98f0bde1f86d5527f9fb760f95ed22d39321b
chun1 <arthur@chunai.dev>
2024-03-04 14:04:35 +1100

    finish initial publishing and blog upload and display

--- src/pages/[user]/[post].tsx
+++ src/pages/[user]/[post].tsx
@@ -4,6 +4,7 @@ import { api } from "~/utils/api";
 import { Navbar } from "~/components/ui/Navbar";
 import { ProfilePic } from "~/components/ui/profile-pic";
 import { Separator } from "@radix-ui/react-dropdown-menu";
+import { editorStyling } from "~/components/ui/Editor";
 
 export default function Page() {
   // TODO layouts
@@ -25,9 +26,9 @@ export default function Page() {
 
   return (
     <>
-      <p>Post: {router.query.user}</p>
-      <p>Post: {router.query.post}</p>
-      <pre>{JSON.stringify(data, null, 4)}</pre>
+      {/* <p>Post: {router.query.user}</p>
+      <p>Post: {router.query.post}</p> */}
+      {/* <pre>{JSON.stringify(data, null, 4)}</pre> */}
       <main className="container max-w-[80ch] py-8">
         <h1 className="py-4 text-5xl font-semibold leading-5">{data?.title}</h1>
         <div className="flex items-center justify-start py-4">
@@ -45,29 +46,10 @@ export default function Page() {
             </p>
           </div>
         </div>
-        <Separator className="h-1 bg-gray-400" />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, laborum
-        consectetur optio tenetur reprehenderit odit quas eius modi veniam
-        assumenda? Molestias fugiat aut quae? Aperiam veniam, illo ab maiores
-        iure commodi similique fugiat vero ullam corporis quae quisquam mollitia
-        illum laudantium quibusdam harum nam sed odit! Et, eos laboriosam
-        laborum vel unde dolorum pariatur id voluptates iure quo sunt
-        reprehenderit. Quasi, delectus. Accusantium dicta necessitatibus
-        asperiores alias temporibus adipisci commodi unde numquam dignissimos
-        consectetur, id doloremque! Tenetur nihil repudiandae aut sed fugit
-        tempora, tempore iure saepe velit enim, nostrum odit placeat alias
-        similique quia possimus consequuntur vero quisquam, repellat temporibus
-        iste earum. Quisquam, quos, placeat minima autem aspernatur laudantium
-        sapiente incidunt unde pariatur, fugiat accusamus. Molestiae velit
-        asperiores sit reiciendis explicabo illum, totam voluptatum atque
-        corrupti cumque hic maxime cum rem alias? Possimus magni doloremque
-        aliquid dolorem, numquam odio culpa est dolores et neque dolorum
-        reiciendis? Eos hic a, fuga molestias velit est quisquam incidunt nihil
-        ex repudiandae autem voluptatem doloribus ullam nesciunt numquam
-        corrupti quod dolor error amet. Provident possimus exercitationem,
-        cumque aperiam, inventore totam architecto et dicta impedit velit
-        voluptates, molestiae a cum porro quo hic corrupti ipsa. Accusantium
-        accusamus facere nisi asperiores nemo! Modi sint repellat ad?
+        <div
+          className={editorStyling}
+          dangerouslySetInnerHTML={{ __html: data.content }}
+        />
       </main>
     </>
   );
