ace2edd7f27192b7c3dd573796d3d4876f6dfc09
chun1 <arthur@chunai.dev>
2024-03-04 14:08:13 +1100

    add popup publish

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -156,3 +156,75 @@ function Layout({ children }: { children: ComponentType }) {
 }
 
 Home.Layout = Layout;
+
+import { CategorySchema } from "~/utils/types";
+
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+function UploadPage({ user }: { user: Undefinable<Session["user"]> }) {
+  const { html } = useStore(useEditorStore);
+
+  const { data, mutate: createNewPost } = api.post.createPost.useMutation();
+  // const user = sessionData.user;
+  console.log("user", user);
+  // console.log("userPosts", userPosts);
+  console.log("data", data);
+  console.log("html", html);
+  const formSchema = z.object({
+    // name: z.string().min(6),
+    // category: z.string().min(1),
+    title: z.string().min(6),
+    category: CategorySchema,
+    content: z.string().min(6),
+    tags: z.array(z.string()),
+    published: z.boolean(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
