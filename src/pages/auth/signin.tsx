7a526caff9c20560a192e435cd0289ba0b850e3b
chun1 <arthur@chunai.dev>
2024-03-05 19:15:19 +1100

    clean up auth

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -5,18 +5,9 @@ import Link from "next/link";
 import { cn } from "~/lib/utils";
 
 import * as React from "react";
-import { IconKey, Icons } from "~/components/icons";
 import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
-import type {
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-} from "next";
 
-import { getProviders, signIn } from "next-auth/react";
-import { getServerSession } from "next-auth/next";
-import { authOptions, getServerAuthSession } from "~/server/auth";
+import { signIn } from "next-auth/react";
 import { api } from "~/utils/api";
 
 export default function AuthForm({
@@ -34,31 +25,6 @@ export default function AuthForm({
   console.log(providers);
   return (
     <div className={cn("container grid max-w-md gap-6", className)} {...props}>
-      {/* <form onSubmit={onSubmit}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
-          </div>
-          <Button disabled={isLoading}>
-            {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Sign In with Email
-          </Button>
-        </div>
-      </form> */}
-
       <h1 className="text-center font-serif text-3xl">Welcome Back </h1>
       {providers &&
         Object.values(providers).map((provider) => (
@@ -70,10 +36,6 @@ export default function AuthForm({
             key={provider.name}
             onClick={() => signIn(provider.id, { callbackUrl: "/" })}
           >
-            {/* <Icons className="mr-2 h-4 w-4" /> */}
-            {/* {Icons[provider.id as IconKey]
-              ? Icons[provider.id as IconKey]({ className: "mr-2 h-4 w-4" })
-              : null} */}
             {
               <Image
                 src={provider.logoUrl}
@@ -89,14 +51,6 @@ export default function AuthForm({
     </div>
   );
 }
-
-// function Signup() {
-
-//   return (
-//     <>
-//     </>
-//   );
-// }
 
 AuthForm.Layout = function Layout({ children }: { children: React.ReactNode }) {
   // remove the layout
