fcad842320de62caf1a6712d5c44cde37da81809
chun1 <arthur@chunai.dev>
2024-03-06 08:52:53 +1100

    refactor signin, fix styles

---
+++ src/components/ui/AuthForm.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "~/lib/utils";
+import * as React from "react";
+import { Button } from "~/components/ui/button";
+import { signIn } from "next-auth/react";
+import { api } from "~/utils/api";
+
+export default function AuthForm({
+  mode,
+  className,
+  ...props
+}: {
+  mode: "signin" | "signup";
+} & React.HTMLAttributes<HTMLDivElement>) {
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+  }
+  const { data: providers } = api.user.getProviders.useQuery();
+  const classOveride: Record<string, string> = {
+    discord: "bg-[#5865F2] text-white",
+  } as const;
+
+  console.log(providers);
+  return (
+    <main className="flex h-screen w-screen items-center justify-center ">
+      <div
+        className={cn(
+          "container flex w-full max-w-xl flex-col items-center justify-center gap-6 p-16  text-center shadow-sm ",
+          className,
+        )}
+        {...props}
+      >
+        <h1 className=" font-serif text-3xl">Welcome Back. </h1>
+        {providers &&
+          Object.values(providers).map((provider) => (
+            <Button
+              variant="outline"
+              //if id in backgroundOveride then use it
+              className={"w-80 justify-between " + classOveride[provider.id]}
+              type="button"
+              key={provider.name}
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              {
+                <Image
+                  src={provider.logoUrl}
+                  alt={provider.name}
+                  width={24}
+                  height={24}
+                />
+              }
+              {mode == "signin" ? "Sign in" : "Sign up"} with {provider.name}
+              <span></span>
+            </Button>
+          ))}
+        {/* no account create one */}
+        <span>
+          No account yet?{" "}
+          <Link
+            href={`/auth/${mode}`}
+            className=" font-semibold text-green-700"
+          >
+            Create One
+          </Link>
+        </span>
+      </div>
+    </main>
+  );
+}
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,62 +1,11 @@
 import { Metadata } from "next";
-import Image from "next/image";
-import Link from "next/link";
-
-import { cn } from "~/lib/utils";
-
-import * as React from "react";
-import { Button } from "~/components/ui/button";
-
-import { signIn } from "next-auth/react";
-import { api } from "~/utils/api";
-
-export default function AuthForm({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault();
-  }
-  const { data: providers } = api.user.getProviders.useQuery();
-  const classOveride: Record<string, string> = {
-    discord: "bg-[#5865F2] text-white",
-  } as const;
-
-  console.log(providers);
-  return (
-    <div className={cn("container grid max-w-md gap-6", className)} {...props}>
-      <h1 className="text-center font-serif text-3xl">Welcome Back </h1>
-      {providers &&
-        Object.values(providers).map((provider) => (
-          <Button
-            variant="outline"
-            //if id in backgroundOveride then use it
-            className={"justify-between " + classOveride[provider.id]}
-            type="button"
-            key={provider.name}
-            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-          >
-            {
-              <Image
-                src={provider.logoUrl}
-                alt={provider.name}
-                width={24}
-                height={24}
-              />
-            }
-            Sign in with {provider.name}
-            <span></span>
-          </Button>
-        ))}
-    </div>
-  );
+import AuthForm from "~/components/ui/AuthForm";
+function Page() {
+  return <AuthForm mode="signin" />;
 }
+export default Page;
 
-AuthForm.Layout = function Layout({ children }: { children: React.ReactNode }) {
+Page.Layout = function Layout({ children }: { children: React.ReactNode }) {
   // remove the layout
-  return (
-    <>
-      <main>{children}</main>
-    </>
-  );
+  return <>{children}</>;
 };
