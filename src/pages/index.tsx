5a6ede84de12835aec232303d5780274119e4f1f
chun1 <arthur@chunai.dev>
2024-03-06 08:18:50 +1100

    add category filter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { IoMdNotifications as BellOpen } from "react-icons/io";
 
-import { ReactElement, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 
 import { api } from "~/utils/api";
 // import { SplashBackground } from "../components/svgs/SplashBackground";
@@ -13,22 +13,14 @@ import { Navbar } from "~/components/ui/Navbar";
 import { Separator } from "@radix-ui/react-dropdown-menu";
 import { ProfilePic } from "../components/ui/profile-pic";
 
-import { JSDOM } from "jsdom";
+import { twMerge } from "tailwind-merge";
 export default function Home() {
-  const blogs = api.post.prototype.useQuery();
-  const [blogPost, setBlogPost] = useState<Post[]>([]);
-
   const { data: sessionData } = useSession();
   // const domParser = new DOMParser();
   // const domParser = new JSDOM().window;
   // need this to only run on the client
   // const [domParser, setUseDomParser] = useState(() => new DOMParser());
 
-  const { data: secretMessage } = api.post.checkSecret.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-  const categories = Object.values(Category);
   return (
     <>
       <Head>
@@ -39,96 +31,7 @@ export default function Home() {
       {sessionData ? (
         <>
           <Navbar />
-          <main className="container border-t-[1px] border-gray-200 ">
-            <div className="grid min-h-screen lg:grid-cols-[1fr_368px]">
-              <main className="px-14">
-                <TestPost />
-                <p>{secretMessage}</p>
-                {/* <Separator /> */}
-                {blogs.data?.map((post) => (
-                  <div
-                    key={post.id}
-                    className="   flex flex-col border-y-[1px] border-gray-100 pt-6"
-                  >
-                    <div className="flex items-center gap-2  ">
-                      {/* profile pic */}
-
-                      <Link
-                        href={`/user/${post.author.name}`}
-                        className="flex gap-1"
-                      >
-                        <ProfilePic author={post.author} className="h-6 w-6" />
-                        <p className="">{post.author.name}</p>
-                      </Link>
-                      <span className="pb-2">.</span>
-                      <span className="text-gray-500">
-                        {post.createdAt.toLocaleDateString("en-US", {
-                          year: "numeric",
-                          month: "long",
-                          day: "numeric",
-                        })}
-                      </span>
-                    </div>
-
-                    <div className="flex h-[208px] justify-between ">
-                      <Link
-                        href={`/${post.author.name}/${post.id}`}
-                        // TODO figure out whether I can use title and
-                        // whether it will automatically be slugified
-                        className=""
-                      >
-                        <h1 className="text-2xl font-semibold">{post.title}</h1>
-                        <div className="line-clamp-4 ">{post.subtitle}</div>
-                        <li className="flex gap-3  py-8 ">
-                          {/* this extra flex container needed otherwise it grows full width of the card */}
-                          <Link
-                            href={`/category/${post.category}`}
-                            className=" flex-shrink-0 items-center justify-center rounded-full bg-gray-100 px-2 py-1 text-sm"
-                          >
-                            {post.category.replace(/([a-z])([A-Z])/g, "$1 $2")}
-                          </Link>
-                          <Link href={`/${post.author.name}/${post.id}`}>
-                            {/* TODO figure out why this is wrong  */}
-                            {Math.ceil(post?.nwords ?? 0 / 200)} min read
-                          </Link>
-                          <Link href={`/${post.author.name}/${post.id}`}>
-                            {Math.random() > 0.3 ? (
-                              <span>Selected for you</span>
-                            ) : null}
-                          </Link>
-                        </li>
-                      </Link>
-                      {/* lorem picsum  110*110*/}
-                      <img
-                        src="https://picsum.photos/id/237/110/110"
-                        alt=""
-                        className="ml-16 h-28 w-28"
-                      />
-                    </div>
-                  </div>
-                ))}
-              </main>
-              <aside className="hidden border-l-[1px] border-gray-200 lg:flex">
-                {/* map categories into pill buttons a tags */}
-                <div className="flex flex-col gap-5 p-4">
-                  <h3 className="text-2xl font-semibold">Reccomended topics</h3>
-                  <ul className="flex  flex-wrap gap-6">
-                    {categories.splice(0, 10).map((category) => (
-                      <li key={category}>
-                        <Link
-                          href={`category/${category}`}
-                          className="rounded-full  bg-gray-100 p-3 text-base"
-                        >
-                          {/* split camelcase TODO need to understand this regex */}
-                          {category.replace(/([a-z])([A-Z])/g, "$1 $2")}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </aside>
-            </div>
-          </main>
+          <Main />
         </>
       ) : (
         <>
@@ -175,9 +78,173 @@ export default function Home() {
             {/* get new blogpost */}
             {/* <button onClick */}
           </main>
+          <Main />
         </>
       )}
     </>
+  );
+}
+
+function Main() {
+  const blogs = api.post.prototype.useQuery();
+  const [blogPost, setBlogPost] = useState<Post[]>([]);
+  const [filteredCategories, setFilteredCategories] = useState<Category[]>([]);
+  // const { data: secretMessage } = api.post.checkSecret.useQuery(
+  //   undefined, // no input
+  //   { enabled: sessionData?.user !== undefined },
+  // );
+  // useEffect to detect Esc key press to clear filter categories
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      // console.log("kd", event.key);
+      if (event.key === "Escape") {
+        // console.log("esc");
+        setFilteredCategories([]);
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const categories = Object.values(Category);
+  return (
+    <main className="container border-t-[1px] border-gray-200 ">
+      <div className="grid min-h-screen lg:grid-cols-[1fr_368px]">
+        <main className="px-14">
+          {/* <TestPost /> */}
+          {/* <p>{secretMessage}</p> */}
+          {/* <Separator /> */}
+          {blogs.data
+            ?.filter((post) => {
+              if (filteredCategories.length === 0) {
+                return true;
+              }
+              return filteredCategories.includes(post.category);
+            })
+
+            .map((post) => (
+              <div
+                key={post.id}
+                className="   flex flex-col border-y-[1px] border-gray-100 pt-6"
+              >
+                <div className="flex items-center gap-2  ">
+                  {/* profile pic */}
+
+                  <Link
+                    href={`/user/${post.author.name}`}
+                    className="flex gap-1"
+                  >
+                    <ProfilePic author={post.author} className="h-6 w-6" />
+                    <p className="">{post.author.name}</p>
+                  </Link>
+                  <span className="pb-2">.</span>
+                  <span className="text-gray-500">
+                    {post.createdAt.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </span>
+                </div>
+
+                <div className="flex h-[208px] justify-between ">
+                  <Link
+                    href={`/${post.author.name}/${post.id}`}
+                    // TODO figure out whether I can use title and
+                    // whether it will automatically be slugified
+                    className=""
+                  >
+                    <h1 className="text-2xl font-semibold">{post.title}</h1>
+                    <div className="line-clamp-4 ">{post.subtitle}</div>
+                    <li className="flex items-center   gap-3 py-8 text-sm">
+                      {/* this extra flex container needed otherwise it grows full width of the card */}
+                      {
+                        <CategoryFilterPill
+                          setFilteredCategories={setFilteredCategories}
+                          filteredCategories={filteredCategories}
+                          category={post.category}
+                          className=" items-centebg-gray-100r flex-shrink-0 justify-center rounded-full bg-gray-100 px-2 py-1 text-sm"
+                        />
+                      }
+
+                      <div className="flex gap-2 text-gray-500">
+                        <Link href={`/${post.author.name}/${post.id}`}>
+                          {/* TODO figure out why this is wrong  */}
+                          {Math.ceil(post?.nwords ?? 0 / 200)} min read
+                        </Link>
+                        ·
+                        <Link href={`/${post.author.name}/${post.id}`}>
+                          {Math.random() > 0.1 ? (
+                            <span>Selected for you</span>
+                          ) : null}
+                        </Link>
+                      </div>
+                    </li>
+                  </Link>
+                  {/* lorem picsum  110*110*/}
+                  <img
+                    src="https://picsum.photos/id/237/110/110"
+                    alt=""
+                    className="ml-16 h-28 w-28"
+                  />
+                </div>
+              </div>
+            ))}
+        </main>
+        <aside className="hidden border-l-[1px] border-gray-200 lg:flex">
+          {/* map categories into pill buttons a tags */}
+          <div className="flex flex-col gap-5 p-4">
+            <h3 className="text-2xl font-semibold">Reccomended topics</h3>
+            <ul className="flex  flex-wrap gap-6">
+              {categories.splice(0, 10).map((category) => (
+                <CategoryFilterPill
+                  key={category}
+                  setFilteredCategories={setFilteredCategories}
+                  filteredCategories={filteredCategories}
+                  category={category}
+                />
+              ))}
+            </ul>
+          </div>
+        </aside>
+      </div>
+    </main>
+  );
+}
+
+function CategoryFilterPill({
+  setFilteredCategories,
+  filteredCategories,
+  category,
+  className,
+}: {
+  setFilteredCategories: (categories: Category[]) => void;
+  filteredCategories: Category[];
+  category: Category;
+} & React.HTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      className={twMerge(
+        ` flex-shrink-0 items-center justify-center rounded-full  px-2 py-1 text-sm ${
+          filteredCategories.includes(category) ? "bg-gray-400" : "bg-gray-100"
+        } `,
+        className,
+      )}
+      onClick={() => {
+        if (filteredCategories.includes(category)) {
+          setFilteredCategories(
+            filteredCategories.filter((c) => c !== category),
+          );
+          return;
+        }
+
+        setFilteredCategories([...filteredCategories, category]);
+      }}
+    >
+      {category.replace(/([a-z])([A-Z])/g, "$1 $2")}
+    </button>
   );
 }
 
