1aafcc1b760d40f1e31ee53943bb92e644dbbcaf
chun1 <arthur@chunai.dev>
2024-03-25 12:06:55 +1100

    fix styling letter for icon

--- src/components/ui/profile-pic.tsx
+++ src/components/ui/profile-pic.tsx
@@ -4,6 +4,12 @@ import Link from "next/link";
 import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 import { Undefinable } from "~/utils/types";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "~/components/ui/hover-card";
+
 export function ProfilePic({
   author,
   className, // for tailwind classes but mainly for h and w
@@ -12,30 +18,32 @@ export function ProfilePic({
     return null;
   }
 
-  return author.image ? (
-    <Image
-      src={author.image} //?? "https://via.placeholder.com/150"}
-      alt="profile picture"
-      // layout="fill"
-      // objectFit="cover"
-      width={24}
-      height={24}
-      className={twMerge(
-        "mr-2  h-6 w-6 overflow-hidden rounded-full ",
-        className,
-      )}
-    />
-  ) : (
+  return (
     <div
-      className={twMerge("text-xl font-bold text-white", className)}
-      // random hue background
-      // todo change this to image element
       style={{
+        // actually can't be random need a deterministic way to generate colors from the name
         background: `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`,
       }}
+      className={twMerge(
+        " mr-2 h-6 w-6  overflow-hidden rounded-full text-center font-bold text-white ",
+        className,
+      )}
     >
       {/*Placeholder same as youtube  */}
-      {author?.name?.[0] ?? ""}
+      {author.image ? (
+        <Image
+          src={author.image} //?? "https://via.placeholder.com/150"}
+          alt="profile picture"
+          // layout="fill"
+          // objectFit="cover"
+          width={24}
+          height={24}
+          className={twMerge(className)}
+        />
+      ) : (
+        // get first alphabetcal letter
+        author.name?.split("").find((char) => char?.match(/[a-zA-Z]/)) ?? ""
+      )}
     </div>
   );
 }
